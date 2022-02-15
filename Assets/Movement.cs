f96acb84f08e9cec55208658e105a66e26341b74
Andrey <andreybabich2015@yandex.ru>
2022-02-16 01:38:14 +0300

    Adaptive borders for different resolutions

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -8,30 +8,33 @@ public class Movement : MonoBehaviour
 
     
     private Rigidbody2D rb;
-    
+    private float lrBoarder;
+    private float udBoarder;
     private float movespeed = 0.0065f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        
+        Vector3 screenSize = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        lrBoarder = screenSize.x - (transform.localScale.x / 2);
+        udBoarder = screenSize.y - (transform.localScale.y / 2);
         Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
             rb.position = touch.deltaPosition * movespeed + rb.position;
-            if(rb.position.x > 2f) rb.position = new Vector2(2f, rb.position.y);
-            if(rb.position.x < -2f) rb.position = new Vector2(-2f, rb.position.y);
-            if(rb.position.y > 4.75f) rb.position = new Vector2(rb.position.x, 4.75f);
-            if(rb.position.y < -4.75f) rb.position = new Vector2(rb.position.x, -4.75f);
-            Debug.Log(rb.position);
-            
+            if (rb.position.x > lrBoarder) rb.position = new Vector2(lrBoarder, rb.position.y);
+            if (rb.position.x < -lrBoarder) rb.position = new Vector2(-lrBoarder, rb.position.y);
+            if (rb.position.y > udBoarder) rb.position = new Vector2(rb.position.x, udBoarder);
+            if(rb.position.y < -udBoarder) rb.position = new Vector2(rb.position.x, -udBoarder);
+
         }
     }
 }
