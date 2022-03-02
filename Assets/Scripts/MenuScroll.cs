9a86148e8eb11c7443fa5a51c95caa5c7e4c288d
Andrey <andreybabich2015@yandex.ru>
2022-03-02 20:10:53 +0300

    Added animations in main menu

---
+++ Assets/Scripts/MenuScroll.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScroll : MonoBehaviour
+{
+
+    public float scrollSpeed;
+    public float tileSize = 17;
+    private Transform curentObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        curentObject = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        curentObject.position = new Vector3(curentObject.position.x, -Mathf.Repeat(Time.time * scrollSpeed, tileSize),
+            curentObject.position.z);
+            
+    }
+}
