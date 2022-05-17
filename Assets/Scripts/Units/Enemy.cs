3924ded4f448e38256c945ff9e187ba29620482e
AndreyBP <andreybabich2012@yandex.ru>
2022-05-17 23:17:22 +0300

    Enemy_1 code added

--- Assets/Scripts/Units/Enemy.cs
+++ Assets/Scripts/Units/Enemy.cs
@@ -1,14 +1,36 @@
+using System;
 using UnityEngine;
+using Debug = System.Diagnostics.Debug;
 
-public class Enemy : Unit
+public class Enemy : MonoBehaviour
 {
-    protected override void Move()
+    [SerializeField]
+    private float _speed;
+    public GameObject player;
+    private Rigidbody2D _rigidbody2D;
+
+    private void Move()
     {
         
+        GetComponent<Rigidbody2D>().velocity = player.transform.position.normalized * _speed;
     }
 
-    protected override void Attack()
+
+    private void Start()
     {
-        
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Player");
+        Move();
+    }
+
+
+    private void FixedUpdate() => Attack();
+
+    protected  void Attack()
+    {
+        if (player.transform.position.y < gameObject.transform.position.y)
+        {
+            _rigidbody2D.velocity = player.transform.position.normalized * _speed;
+        }
     }
 }
--- Assets/Scripts/Weapons/Blaster.cs
+++ Assets/Scripts/Weapons/Blaster.cs
@@ -25,6 +25,5 @@ public class Blaster : Weapon
     {
         audioSource.Play();
         audioSource.PlayOneShot(audioSource.clip);
-        // audioSource.PlayOneShot(soundOfShot);
     }
 }
